7a20d8253886ae811f5b6dfc5b67d4b29acfe1b8
michael-lee-bubble <michael.lee@bubble.io>
2023-12-26 14:45:57 -0500

    Add new files.

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
+from flask import Flask, render_template, request
+from app_functions.text_manipulations import lowercase, uppercase
+from app_functions.math_functions import add, subtract, multiply, divide
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask! This is my first deploy :)'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    results = {}
+    if request.method == 'POST':
+        # Text Manipulation Functions
+        if 'lowercase' in request.form:
+            text = request.form['input_lowercase']
+            results['lowercase'] = lowercase(text)
+        elif 'uppercase' in request.form:
+            text = request.form['input_uppercase']
+            results['uppercase'] = uppercase(text)
+        
+        # Math Functions
+        # Note: Convert inputs to appropriate types (e.g., int or float)
+        if 'add' in request.form:
+            num1 = float(request.form['input_add1'])
+            num2 = float(request.form['input_add2'])
+            results['add'] = add(num1, num2)
+        # ... Similarly handle subtract, multiply, divide
+
+    return render_template('index.html', results=results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
