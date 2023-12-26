9d4ec8fae67c44b46127fcf4e549c664e10be7fa
michael-lee-bubble <michael.lee@bubble.io>
2023-12-26 16:06:26 -0500

    Add text_manipulations functions.

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from app_functions.text_manipulations import lowercase, uppercase
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    result = None
+    if request.method == 'POST':
+        operation = request.form.get('operation')
+        text = request.form.get('text')
+        if operation == 'lower':
+            result = lowercase(text)
+        elif operation == 'upper':
+            result = uppercase(text)
+    return render_template('index.html', result=result)
 
 if __name__ == '__main__':
     app.run()
