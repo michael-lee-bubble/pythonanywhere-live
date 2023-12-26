83cb2eb9531eca9c0540a9bdd5060612eb7f9a31
michael-lee-bubble <michael.lee@bubble.io>
2023-12-26 16:09:33 -0500

    Add math_functions file.

--- app.py
+++ app.py
@@ -1,19 +1,37 @@
 from flask import Flask, render_template, request
 from app_functions.text_manipulations import lowercase, uppercase
+from app_functions.math_functions import add, subtract, divide, multiply
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    result = None
+    text_result = None
+    math_result = None
+
     if request.method == 'POST':
-        operation = request.form.get('operation')
-        text = request.form.get('text')
-        if operation == 'lower':
-            result = lowercase(text)
-        elif operation == 'upper':
-            result = uppercase(text)
-    return render_template('index.html', result=result)
+        if 'operation' in request.form:
+            operation = request.form.get('operation')
+            text = request.form.get('text')
+            if operation == 'lower':
+                text_result = lowercase(text)
+            elif operation == 'upper':
+                text_result = uppercase(text)
+
+        elif 'operation_math' in request.form:
+            operation_math = request.form.get('operation_math')
+            input_math1 = float(request.form.get('input_math1'))
+            input_math2 = float(request.form.get('input_math2'))
+            if operation_math == 'add':
+                math_result = add(input_math1, input_math2)
+            elif operation_math == 'subtract':
+                math_result = subtract(input_math1, input_math2)
+            elif operation_math == 'multiply':
+                math_result = multiply(input_math1, input_math2)
+            elif operation_math == 'divide':
+                math_result = divide(input_math1, input_math2)
+
+    return render_template('index.html', text_result=text_result, math_result=math_result)
 
 if __name__ == '__main__':
     app.run()
--- app_functions/math_functions.py
+++ app_functions/math_functions.py
@@ -1,13 +1,15 @@
 import sys
 
-def add(input_1,input_2):
+def add(input_1, input_2):
     return input_1 + input_2
 
-def subtract(input_1,input_2):
+def subtract(input_1, input_2):
     return input_1 - input_2
 
-def divide(input_1,input_2):
-    return input_1/input_2
+def divide(input_1, input_2):
+    if input_2 == 0:
+        return "Cannot divide by zero"
+    return input_1 / input_2
 
-def multiply(input_1,input_2):
-    return input_1*input_2
+def multiply(input_1, input_2):
+    return input_1 * input_2
