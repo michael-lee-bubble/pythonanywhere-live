a7f8f893936834e3cd721a09717fca901f50c51e
michael-lee-bubble <michael.lee@bubble.io>
2023-12-26 16:23:18 -0500

    Add updates to math_functions file.

--- app_functions/math_functions.py
+++ app_functions/math_functions.py
@@ -13,3 +13,21 @@ def divide(input_1, input_2):
 
 def multiply(input_1, input_2):
     return input_1 * input_2
+
+if len(sys.argv) == 4:
+    operation = sys.argv[1].lower()
+    input_1 = float(sys.argv[2])
+    input_2 = float(sys.argv[3])
+
+    if operation == 'add':
+        print(add(input_1, input_2))
+    elif operation == 'subtract':
+        print(subtract(input_1, input_2))
+    elif operation == 'multiply':
+        print(multiply(input_1, input_2))
+    elif operation == 'divide':
+        print(divide(input_1, input_2))
+    else:
+        print("Invalid operation. Use 'add', 'subtract', 'multiply', or 'divide'.")
+else:
+    print("Usage: python math_functions.py <operation> <input_1> <input_2>")
