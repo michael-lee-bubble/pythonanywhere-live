d0b4844463da4d458506d0414ae6b45d4a9f0bef
michael-lee-bubble <michael.lee@bubble.io>
2023-12-27 17:26:36 -0500

    Add sessions changes.

--- app.py
+++ app.py
@@ -1,30 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 from app_functions.text_manipulations import lowercase, uppercase
 from app_functions.math_functions import add, subtract, divide, multiply
+from dotenv import load_dotenv
+load_dotenv()  # This loads the variables from .env
+import os
+
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('FLASK_SECRET_KEY', 'default-secret-key')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # Initialize or retrieve previous results and selections
-    text_result = None
-    math_result = None
-    selected_text_operation = None
-    selected_math_operation = None
-
     if request.method == 'POST':
         # Process Text Manipulation Form
         if 'text_submit' in request.form:
             selected_text_operation = request.form.get('operation')
             text = request.form.get('text')
             if selected_text_operation == 'lower':
-                text_result = lowercase(text)
+                session['text_result'] = lowercase(text)
             elif selected_text_operation == 'upper':
-                text_result = uppercase(text)
-        else:
-            # Retrieve previous text operation and result
-            selected_text_operation = request.form.get('operation', None)
-            text_result = request.form.get('text_result', None)
+                session['text_result'] = uppercase(text)
+            session['selected_text_operation'] = selected_text_operation
 
         # Process Math Calculation Form
         if 'math_submit' in request.form:
@@ -35,17 +31,23 @@ def index():
                 input_math1 = float(input_math1)
                 input_math2 = float(input_math2)
                 if selected_math_operation == 'add':
-                    math_result = add(input_math1, input_math2)
+                    session['math_result'] = add(input_math1, input_math2)
                 elif selected_math_operation == 'subtract':
-                    math_result = subtract(input_math1, input_math2)
+                    session['math_result'] = subtract(input_math1, input_math2)
                 elif selected_math_operation == 'multiply':
-                    math_result = multiply(input_math1, input_math2)
+                    session['math_result'] = multiply(input_math1, input_math2)
                 elif selected_math_operation == 'divide':
-                    math_result = divide(input_math1, input_math2)
-        else:
-            # Retrieve previous math operation and result
-            selected_math_operation = request.form.get('operation_math', None)
-            math_result = request.form.get('math_result', None)
+                    session['math_result'] = divide(input_math1, input_math2)
+            session['selected_math_operation'] = selected_math_operation
+
+        # Redirect to prevent form resubmission
+        return redirect(url_for('index'))
+
+    # For a GET request, retrieve results and selections from the session
+    text_result = session.get('text_result')
+    math_result = session.get('math_result')
+    selected_text_operation = session.get('selected_text_operation')
+    selected_math_operation = session.get('selected_math_operation')
 
     return render_template('index.html', 
                            text_result=text_result, 
