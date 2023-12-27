ec0c94126b9f9a0a558defb68a1d37900f2902c2
michael-lee-bubble <michael.lee@bubble.io>
2023-12-27 12:30:26 -0500

    Add saved stated for dropdowns.

--- app.py
+++ app.py
@@ -8,30 +8,36 @@ app = Flask(__name__)
 def index():
     text_result = None
     math_result = None
+    selected_text_operation = None
+    selected_math_operation = None
 
     if request.method == 'POST':
         if 'text_submit' in request.form:  # Check if text manipulation button was clicked
-            operation = request.form.get('operation')
+            selected_text_operation = request.form.get('operation')
             text = request.form.get('text')
-            if operation == 'lower':
+            if selected_text_operation == 'lower':
                 text_result = lowercase(text)
-            elif operation == 'upper':
+            elif selected_text_operation == 'upper':
                 text_result = uppercase(text)
 
         elif 'math_submit' in request.form:  # Check if math button was clicked
-            operation_math = request.form.get('operation_math')
+            selected_math_operation = request.form.get('operation_math')
             input_math1 = float(request.form.get('input_math1'))
             input_math2 = float(request.form.get('input_math2'))
-            if operation_math == 'add':
+            if selected_math_operation == 'add':
                 math_result = add(input_math1, input_math2)
-            elif operation_math == 'subtract':
+            elif selected_math_operation == 'subtract':
                 math_result = subtract(input_math1, input_math2)
-            elif operation_math == 'multiply':
+            elif selected_math_operation == 'multiply':
                 math_result = multiply(input_math1, input_math2)
-            elif operation_math == 'divide':
+            elif selected_math_operation == 'divide':
                 math_result = divide(input_math1, input_math2)
 
-    return render_template('index.html', text_result=text_result, math_result=math_result)
+    return render_template('index.html', 
+                           text_result=text_result, 
+                           math_result=math_result,
+                           selected_text_operation=selected_text_operation,
+                           selected_math_operation=selected_math_operation)
 
 if __name__ == '__main__':
     app.run()
