3943cd2a1a1b676ae87746c6d22fc51150c6485f
michael-lee-bubble <michael.lee@bubble.io>
2023-12-27 12:43:00 -0500

    Fix saved state for results.

--- app.py
+++ app.py
@@ -1,35 +1,37 @@
 from flask import Flask, render_template, request
 from app_functions.text_manipulations import lowercase, uppercase
-from app_functions.math_functions import add, subtract, divide, multiply  # Import math functions
+from app_functions.math_functions import add, subtract, divide, multiply
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    # Initialize or retrieve previous results and selections
     text_result = None
     math_result = None
     selected_text_operation = None
     selected_math_operation = None
 
-    # Check for existing session data (if you're using sessions to store previous state)
-    # If not using sessions, you can initialize these to default values or based on some other logic
-
     if request.method == 'POST':
-        # Text Manipulation Section
-        selected_text_operation = request.form.get('operation', selected_text_operation)
-        text = request.form.get('text', None)
+        # Process Text Manipulation Form
         if 'text_submit' in request.form:
+            selected_text_operation = request.form.get('operation')
+            text = request.form.get('text')
             if selected_text_operation == 'lower':
                 text_result = lowercase(text)
             elif selected_text_operation == 'upper':
                 text_result = uppercase(text)
+        else:
+            # Retrieve previous text operation and result
+            selected_text_operation = request.form.get('operation', None)
+            text_result = request.form.get('text_result', None)
 
-        # Math Calculation Section
-        selected_math_operation = request.form.get('operation_math', selected_math_operation)
-        input_math1 = request.form.get('input_math1', None)
-        input_math2 = request.form.get('input_math2', None)
+        # Process Math Calculation Form
         if 'math_submit' in request.form:
-            if input_math1 and input_math2:  # Ensure that both inputs are provided
+            selected_math_operation = request.form.get('operation_math')
+            input_math1 = request.form.get('input_math1')
+            input_math2 = request.form.get('input_math2')
+            if input_math1 and input_math2:
                 input_math1 = float(input_math1)
                 input_math2 = float(input_math2)
                 if selected_math_operation == 'add':
@@ -40,6 +42,10 @@ def index():
                     math_result = multiply(input_math1, input_math2)
                 elif selected_math_operation == 'divide':
                     math_result = divide(input_math1, input_math2)
+        else:
+            # Retrieve previous math operation and result
+            selected_math_operation = request.form.get('operation_math', None)
+            math_result = request.form.get('math_result', None)
 
     return render_template('index.html', 
                            text_result=text_result, 
@@ -48,4 +54,4 @@ def index():
                            selected_math_operation=selected_math_operation)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
