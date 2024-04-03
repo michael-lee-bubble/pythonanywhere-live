c75b3dd458bad253a80817de6fff6ebf902589e9
michael-lee-bubble <michael.lee@bubble.io>
2024-04-03 18:12:33 -0400

    Update function to clean text.

--- modules/api_calls.py
+++ modules/api_calls.py
@@ -8,15 +8,33 @@ def convert_seconds(unix_seconds):
 
 def clean_text(text):
     # Step 1: Find the first instance of "\n\n>" and remove everything after it
-    cutoff_index = text.find("\n\n>")
-    if cutoff_index != -1:  # If "\n\n>" was found
-        text = text[:cutoff_index]
+    cutoff_index_1 = text.find("\n\n>")
+    if cutoff_index_1 != -1:  # If "\n\n>" was found
+        text = text[:cutoff_index_1]
         
         # Step 2: From the end, find the first instance of "\n" and remove everything after that
         # This step only occurs if "\n\n>" was found
-        last_line_index = text.rfind("\n")
-        if last_line_index != -1 and last_line_index != 0:  # Ensure "\n" was found and it's not at the start
-            text = text[:last_line_index]
-    # If "\n\n>" was not found, do nothing and keep the text as is
+        cutoff_index_2 = text.rfind("\n")
+        if cutoff_index_2 != -1 and cutoff_index_2 != 0:  # Ensure "\n" was found and it's not at the start
+            text = text[:cutoff_index_2]
+            
+            # Find additional text to remove
+            cutoff_index_3 = text.rfind(" Bubble Support - ") # If " Bubble Support - " was found
+            if cutoff_index_3 != -1:
+                text = text[:cutoff_index_3]
+
+                # Find additional text to remove
+                cutoff_index_4 = text.rfind("\n")
+                if cutoff_index_4 != -1 and cutoff_index_4 != 0:  # Ensure "\n" was found and it's not at the start
+                    text = text[:cutoff_index_4]
+
+    rating_text_index = text.find("How was the help you received?\n>")
+    if rating_text_index != -1:  # If "How was the help you received?\n>" was found
+        text = text[:rating_text_index]
+
+        # Find additional text to remove
+        cutoff_index_5 = text.rfind("\n")
+        if cutoff_index_5 != -1 and cutoff_index_5 != 0
+            text = text[:cutoff_index_5]
 
     return text
