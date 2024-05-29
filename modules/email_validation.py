a1f51860538096b558f6544d5a179d349da2d674
michael-lee-bubble <michael.lee@bubble.io>
2024-05-29 10:57:20 -0400

    Update email validation.

--- modules/email_validation.py
+++ modules/email_validation.py
@@ -3,6 +3,7 @@ import re
 def is_valid_email(email):
     # Regular expression for validating an email
     email_regex = re.compile(
-        r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+        r"^(?=.{1,256})(?=.{1,64}@.{1,255})[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+"
+        r"(?:\.[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,63}$"
     )
     return re.match(email_regex, email) is not None
